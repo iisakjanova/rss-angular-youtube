a614ba7f645049213b69c89f75ae8856bb59d171
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-11-04 18:26:14 -0500

    refactor: move search results state into youtube service

---
+++ you-tube-client-app/src/app/core/services/youtube/youtube.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { SearchItem } from 'src/app/youtube/components/search-results-block/search-item-model';
+
+import data from '../../../../assets/mock-response.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class YoutubeService {
+  searchItems: SearchItem[] = data.items;
+}
