07cb00c1dd1404d970885dc4b06740a9c13d7eca
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-11-05 23:02:31 -0600

    feat: implement redirection to login page if user is not logged in

--- you-tube-client-app/src/app/app-routing.module.ts
+++ you-tube-client-app/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { authGuard } from './core/guards/auth.guard';
 import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   {
     path: 'youtube',
     loadChildren: () => import('./youtube/youtube.module').then((m) => m.YouTubeModule),
+    canMatch: [authGuard],
   },
   {
     path: 'login',
---
+++ you-tube-client-app/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import {
+  NavigationExtras,
+  Router,
+} from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth/auth.service';
+
+export const authGuard = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  if (authService.isLoggedIn) {
+    return true;
+  }
+
+  const sessionId = 123456789;
+
+  const navigationExtras: NavigationExtras = {
+    queryParams: { session_id: sessionId },
+  };
+
+  return router.createUrlTree(['/login'], navigationExtras);
+};
