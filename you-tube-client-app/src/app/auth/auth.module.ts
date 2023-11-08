fa2a46389788c173c56402c608332b53eae6b9d5
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-11-07 21:11:25 -0600

    fix: change content of auth-routing an auth modules

--- you-tube-client-app/src/app/auth/auth.module.ts
+++ you-tube-client-app/src/app/auth/auth.module.ts
@@ -1,28 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: LoginPageComponent,
-  },
-];
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes),
-    ReactiveFormsModule,
-    CommonModule,
-  ],
-  exports: [RouterModule],
   declarations: [
     LoginPageComponent,
-    LoginFormComponent,
+    LoginFormComponent,],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AuthRoutingModule
   ],
 })
+
 export class AuthModule { }
