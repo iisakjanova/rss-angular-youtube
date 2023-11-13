2c0b4dc0a4f500b929a41b081f8f1eac2939a028
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-11-13 16:48:29 -0600

    feat: implement detailed info page with API

--- you-tube-client-app/src/app/youtube/pages/detailed-page/detailed-page.component.ts
+++ you-tube-client-app/src/app/youtube/pages/detailed-page/detailed-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { YoutubeService } from 'src/app/core/services/youtube/youtube.service';
 
 import type { SearchItem } from '../../components/search-results-block/search-item-model';
+import { YoutubeApiService } from '../../services/youtube/youtube-api.service';
 
 @Component({
   selector: 'app-detailed-page',
@@ -14,7 +15,12 @@ export class DetailedPageComponent implements OnInit {
 
   searchItem: SearchItem | undefined;
 
-  constructor(private router: Router, public youtubeService: YoutubeService, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    public youtubeService: YoutubeService,
+    private route: ActivatedRoute,
+    public youtubeApiService: YoutubeApiService,
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -23,6 +29,18 @@ export class DetailedPageComponent implements OnInit {
 
       if (!this.searchItem) {
         this.router.navigate(['/not-found']);
+      } else {
+        this.youtubeApiService.getVideoDetails([this.itemId]).subscribe({
+          next: (videoDetails) => {
+            if (videoDetails.length > 0) {
+              const [firstItem] = videoDetails[0].items;
+              this.searchItem = firstItem;
+            }
+          },
+          error: (error) => {
+            console.error('Error fetching video details:', error);
+          },
+        });
       }
     });
   }
